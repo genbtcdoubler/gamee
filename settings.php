d1cd23a2677a6cc442d473efdc61c4548b390503
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-02-26 01:57:46 -0800

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$x_install_uuid = "f29ac4e8-80f4-4b67-b317-ca3432a6709c";
+
+$user_agent = "Mozilla/5.0 (Linux; Android 6.0; Lenovo A6600d40) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36";
+
+$refresh_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOiIxNjE2OTIzODY1IiwidXNlcklkIjoyNDA3NTM4MywiaW5zdGFsbFV1aWQiOiJmMjlhYzRlOC04MGY0LTRiNjctYjMxNy1jYTM0MzJhNjcwOWMiLCJ0eXBlIjoicmVmcmVzaFRva2VuIiwiYXV0aG9yaXphdGlvbkxldmVsIjoiZnVsbCIsInBsYXRmb3JtIjoid2ViIn0.7TWxBwARayfMryi6kIQFFoW5pxOuhUf79GqYexhFn5M";
+
+?>
